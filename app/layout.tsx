0e30b8b98fd31ef05396d04d036bbaa3aead0bbb
Aiman Azani <m.aimanazani@gmail.com>
2026-08-11 10:40:45 +0800

    Fix(FavIco): Fix favico on opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,23 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://scrum-x.vercel.app"),
   title: "ScrumX",
   description: "Daily scrum report updates",
   icons: {
     icon: "/scrumx-icon.svg",
+  },
+  openGraph: {
+    title: "ScrumX",
+    description: "Daily scrum report updates",
+    url: "/",
+    siteName: "ScrumX",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ScrumX",
+    description: "Daily scrum report updates",
   },
 };
 
